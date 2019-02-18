a311403c990593d5f873051b0e14ac366c452866
Lucas Marques <lucasmarques73@hotmail.com>
2019-02-18 13:46:17 -0300

    add application

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	port := ":8000"
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/", home).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(port, router))
+
+	fmt.Println("Server running in port:", port)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("templates/index.html"))
+	if err := t.ExecuteTemplate(w, "index.html", nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
